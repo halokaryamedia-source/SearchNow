11bfe94aa57271f2a1f58eeae935948ca96f5290
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-09 18:15:42 +0700

    refactor(tauri): use application bootstrap

--- EngineData/Frontend/RustApp/src-tauri/src/main.rs
+++ EngineData/Frontend/RustApp/src-tauri/src/main.rs
@@ -2,7 +2,8 @@ mod app_bootstrap;
 mod commands;
 
 fn main() {
-    let builder = tauri::Builder::default().setup(|app| app_bootstrap::configure_main_window(app));
+    let builder =
+        tauri::Builder::default().setup(|app| app_bootstrap::configure_application(app));
 
     commands::registry::register(builder)
         .run(tauri::generate_context!())

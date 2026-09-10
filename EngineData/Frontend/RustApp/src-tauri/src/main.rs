295040dbfd0d6050b75131a7e7999629d528fa2d
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-10 22:41:45 +0700

    feat(desktop): initialize native dialog plugin

--- EngineData/Frontend/RustApp/src-tauri/src/main.rs
+++ EngineData/Frontend/RustApp/src-tauri/src/main.rs
@@ -2,7 +2,9 @@ mod app_bootstrap;
 mod commands;
 
 fn main() {
-    let builder = tauri::Builder::default().setup(|app| app_bootstrap::configure_application(app));
+    let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .setup(|app| app_bootstrap::configure_application(app));
 
     commands::registry::register(builder)
         .run(tauri::generate_context!())
